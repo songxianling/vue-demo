4a75fd8cb7ba0817e5e27ddd2d3797d780d5043e
songxl <songxl@bingo>
2017-09-23 17:30:37 +0800

    feat:初始化vue目录

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>首次初始化vue-elemo</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+
+};
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  
+</style>
