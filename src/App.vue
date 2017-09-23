8ae7653a348ac1e4cd9941ba202c35ec2386769d
songxl <songxl@bingo>
2017-09-23 17:49:51 +0800

    feat:header组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <div>首次初始化vue-elemo</div>
+  <div id="app">
+    <v-header></v-header>
+    <div class="header">首次初始化vue-elemo</div>
+    <div class="tab">tab</div>
+    <div class="footer">footer</div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {
+import header from './components/header/header';
 
+export default {
+  // components 注册一个组件
+  components: {
+    'v-header': header
+  }
 };
 
 </script>
