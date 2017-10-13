cf95fdfcfe5017a1cb33c1a033715f0297682f06
songxl <songxl@bingo>
2017-10-13 15:13:56 +0800

    feat:右侧滚动映射左侧菜单实现

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,7 +3,7 @@
     <!-- 左侧菜单 -->
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}">
           <span class="text">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -12,9 +12,9 @@
       </ul>
     </div>
     <!-- 右侧商品 -->
-    <div class="foods-wrapper"  ref="foodsWrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
@@ -53,7 +53,9 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
     };
   },
   created() {
@@ -64,15 +66,45 @@ export default {
         this.goods = res.data;
         this.$nextTick(() => {
           this._initScroll();
+          this._calculateHight();
         });
-       
+
       };
     });
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
+      }
+      return 0;
+    }
   },
   methods: {
     _initScroll() {
       this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
-      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      });
+
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    _calculateHight() {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+      console.log(this.listHeight);
     }
   }
 };
@@ -81,7 +113,7 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/mixin';
 
-.goods 
+.goods {
   display: flex;
   position: absolute;
   top: 174px;
@@ -89,89 +121,143 @@ export default {
   width: 100%;
   overflow: hidden;
 
-  .menu-wrapper 
+  .menu-wrapper {
     flex: 0 0 80px;
     width: 80px;
     background: #f3f5f7;
-    .menu-item
-      display table
-      height 54px
-      line-height 14px
-      width 56px
-      padding 0 12px
-      
-      .icon 
-        display: inline-block
-        width: 16px
-        height: 16px;
-        vertical-align: top
-        margin-right: 2px
-        background-size: 100% 100%
-        background-repeat: no-repeat
 
-        &.decrease 
-          bg-image('decrease_3')
-        &.discount 
-          bg-image('discount_3')
-        &.guarantee 
-          bg-image('guarantee_3')
-        &.invoice 
-          bg-image('invoice_3')
-        &.special 
-          bg-image('special_3')
-      .text  
-        display table-cell
-        width 56px
-        font-size 12px 
-        vertical-align middle 
-        border-1px(rgba(7,17,27,.1))
-  .foods-wrapper 
+    .menu-item {
+      display: table;
+      height: 54px;
+      line-height: 14px;
+      width: 56px;
+      padding: 0 12px;
+
+      &.current {
+        position: relative;
+        z-index: 10;
+        margin-top: -1px;
+        background: #ffffff;
+        font-weight: 700;
+
+        .text {
+          border-none();
+        }
+      }
+
+      .icon {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        vertical-align: top;
+        margin-right: 2px;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+
+        &.decrease {
+          bg-image('decrease_3');
+        }
+
+        &.discount {
+          bg-image('discount_3');
+        }
+
+        &.guarantee {
+          bg-image('guarantee_3');
+        }
+
+        &.invoice {
+          bg-image('invoice_3');
+        }
+
+        &.special {
+          bg-image('special_3');
+        }
+      }
+
+      .text {
+        display: table-cell;
+        width: 56px;
+        font-size: 12px;
+        vertical-align: middle;
+        border-1px(rgba(7, 17, 27, 0.1));
+      }
+    }
+  }
+
+  .foods-wrapper {
     flex: 1;
-    .title
-      padding-left 14px
-      height 26px
-      line-height 26px
-      border-left 2px solid #d9dde1
-      font-size 12px
-      color rgb(147,153,159)
-      background #f3f5f7
-    .food-item
-      display flex
-      margin 18px 
-      padding-bottom 18px
-      border-1px(rgba(7,17,27,.1))
-      &:last-child
-        border-none()
-        margin-bottom 0
-      .icon
-        flex 0 0 57px
-        margin-right 10px
-      .content
-        flex 1
-        .name
-          margin 2px 0 8px 0
-          height 14px
-          line-height 14px
-          font-size 14px
-          color rgb(7,17,27)
-        .desc,.extra
-          height 10px
-          line-height 10px
-          font-size 10px
-          color rgb(147,153,159)
-        .desc
-          margin-bottom 8px
-        .extra>span:nth-child(1)
-          margin-right 12px
-        .price
-          font-weight 700
-          line-height 24px
-          .now
-            margin-right 8px
-            font-size 14px
-            color rgb(240,20,20) 
-          .old
-            text-decoration line-through
-            font-size 10px
-            color rgb(147,153,159)  
+
+    .title {
+      padding-left: 14px;
+      height: 26px;
+      line-height: 26px;
+      border-left: 2px solid #d9dde1;
+      font-size: 12px;
+      color: rgb(147, 153, 159);
+      background: #f3f5f7;
+    }
+
+    .food-item {
+      display: flex;
+      margin: 18px;
+      padding-bottom: 18px;
+      border-1px(rgba(7, 17, 27, 0.1));
+
+      &:last-child {
+        border-none();
+        margin-bottom: 0;
+      }
+
+      .icon {
+        flex: 0 0 57px;
+        margin-right: 10px;
+      }
+
+      .content {
+        flex: 1;
+
+        .name {
+          margin: 2px 0 8px 0;
+          height: 14px;
+          line-height: 14px;
+          font-size: 14px;
+          color: rgb(7, 17, 27);
+        }
+
+        .desc, .extra {
+          line-height: 10px;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+
+        .desc {
+          margin-bottom: 8px;
+          line-height: 12px;
+        }
+
+        .extra>span:nth-child(1) {
+          margin-right: 12px;
+        }
+
+        .price {
+          font-weight: 700;
+          line-height: 24px;
+
+          .now {
+            margin-right: 8px;
+            font-size: 14px;
+            color: rgb(240, 20, 20);
+          }
+
+          .old {
+            text-decoration: line-through;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+        }
+      }
+    }
+  }
+}
 </style>
