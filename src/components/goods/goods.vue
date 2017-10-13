f8ed7cb70ab59564960bfdf979854cc6c0c74d98
songxl <songxl@bingo>
2017-10-13 11:27:45 +0800

    feat:商品内容外壳完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,16 +1,35 @@
 <template>
-  <div>
-    我是goods
+  <div class="goods">
+    <!-- 左侧菜单 -->
+    <div class="menu-wrapper">
+
+    </div>
+    <!-- 右侧商品 -->
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default{
+export default {
 
 };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "../../common/stylus/mixin";
-  
+@import '../../common/stylus/mixin';
+.goods
+  display flex
+  position absolute
+  top 174px
+  bottom 46px
+  width 100%
+  overflow hidden
+
+  .menu-wrapper
+    flex 0 0 80px
+    width 80px
+    background #f3f5f7
+  .foods.wrapper
+    flex 1
+    
 </style>
